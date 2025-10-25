5c552c8fc4d278ab79b59e6a864741b7e5761a03
LenilsonLimaPantoja <lenilsonlm.pantoja@gmail.com>
2025-10-25 00:44:28 -0400

    Alteração

--- src/controllers/peso-caixa-controllers.js
+++ src/controllers/peso-caixa-controllers.js
@@ -226,7 +226,7 @@ exports.getAnaliseOpenAi = async (req, res, next) => {
             Sua tarefa:
             1. Identificar variações anormais (valores muito acima ou abaixo da média).
             2. Determinar a tendência geral do período (crescimento, estabilidade ou queda).
-            3. Gerar observações e possíveis ajustes que o apicultor deve considerar (ao menos 3 registros), e diga o porque.
+            3. Gerar observações e possíveis ajustes que o apicultor deve considerar (ao menos 3 registros), e diga o porque voce fez essa observação.
 
             Regras importantes:
             - Retorne SOMENTE um JSON válido.
